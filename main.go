5dc10aac89936212b6c64890edcdb238c983f1d2
ianchildress <ianchildress@gmail.com>
2015-07-02 13:15:58 -0400

    Direct download of Starter kit lite

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Listening.")
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.Handle("/", http.FileServer(http.Dir("./app")))
 	http.ListenAndServe(":3002", nil)
 
 }
